8528b226101f470daa2af6c1cd6f11089c090468
Robin Cua <robincua@swagsoft.com>
2015-11-13 14:35:12 +0800

    asd

---
+++ sadasd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sadasd
+//
+//  Created by Robin Cua on 13/11/15.
+//  Copyright © 2015 Robin Cua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sadasd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sadasd
+//
+//  Created by Robin Cua on 13/11/15.
+//  Copyright © 2015 Robin Cua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
